27043b12bb7bf6fa1058eff2c15ae71089233cef
Rada Bogdan Raul <raoul_ice@yahoo.com>
2016-08-24 11:24:57 +0300

    sprockets 3 support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,29 @@
+require 'rubygems'
+require 'bundler/setup'
 require 'sprockets'
 require 'sprockets-less'
 require 'sprockets-helpers'
 require 'test_construct'
 
+
+RSpec.configure do |config|
+  config.include TestConstruct::Helpers
+end
+
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
-RSpec.configure do |config|
-  config.include TestConstruct::Helpers
+
+def write_asset(filename, contents, mtime = nil)
+  mtime ||= [Time.now.to_i, File.stat(filename).mtime.to_i].max + 1
+  File.open(filename, 'w') do |f|
+    f.write(contents)
+  end
+  if Sprockets::Less::Utils.version_of_sprockets >= 3
+    File.utime(mtime, mtime, filename)
+  else
+    mtime = Time.now + 1
+    filename.utime mtime, mtime
+  end
 end
